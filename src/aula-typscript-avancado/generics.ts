138cc8d63dd0bc5c0987b6ea3b2e9e0eeffe3fc1
Samara Victoria <Maia_victoria@outlook.com.br>
2025-03-12 21:00:52 -0300

    Criando um repositório de usuários utilizando uma Interface generics e herança de métodos

--- src/aula-typscript-avancado/generics.ts
+++ src/aula-typscript-avancado/generics.ts
@@ -10,3 +10,35 @@ const strings = ["TypeScript", "ao", "vindo", "bem", "Seja"];
 //Imprimindo a resposta no terminal
 console.log(inverterArray(numeros));
 console.log(inverterArray(strings));
+
+//Criando uma interface Genérica
+interface Repositorio<T> {
+    //Definição dos métodos necessários para o repositório
+    salvar(dado: T): void;
+
+    obterTodos(): T[];
+}
+
+//Criando uma outra interface para usuario
+interface Usuario {
+    nome: string;
+    email: string;
+}
+
+//Criando uma classe que implemente um Repositorio de Usuarios
+class UsuarioRepositorio implements Repositorio<Usuario> {
+    private usuarios: Usuario[] = [];
+
+    salvar(dado: Usuario): void {
+        this.usuarios.push(dado);
+    }
+
+    obterTodos(): Usuario[] {
+        return this.usuarios;
+    }
+}
+const repo = new UsuarioRepositorio();
+repo.salvar({ nome: "Alice", email: "alice@fiap.com.br" });
+repo.salvar({ nome: "Samara", email: "santos.samara@gmail.com.br" });
+
+console.log("Usuários:", repo.obterTodos());
